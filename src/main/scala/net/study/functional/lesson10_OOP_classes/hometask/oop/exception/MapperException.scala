2e0d56d917f333357a93ac4104af3476011dbf1c
Petro Kravchuk <petrokravchuk@kyivstar.net>
2023-09-10 10:59:37 +0300

    classwork

---
+++ src/main/scala/net/study/functional/lesson10_OOP_classes/hometask/oop/exception/MapperException.scala
@@ -0,0 +1,4 @@
+package net.study.functional.lesson10_OOP_classes.hometask.oop.exception
+
+// Here you can declare your exceptions
+case class MapperException(errMsg: String) extends RuntimeException(errMsg)
